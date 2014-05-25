44366079615179a78a0f97b2ceedf03c07684001
Ionică Bizău <bizauionica@yahoo.com>
2014-05-25 12:03:15 +0300

    Set my own colors instead

--- test/index.js
+++ test/index.js
@@ -2,4 +2,14 @@ var Flattenize = require ("../index")
   , myImage = new Flattenize ()
   ;
 
+myImage._flatColors.splice(0, myImage._flatColors.length);
+myImage._flatColors.push.apply(myImage._flatColors, [
+    [255, 255, 255] // white
+  , [55,  129, 194] // blue
+  , [77,  183, 73]  // green
+  , [229, 67,  60]  // red
+  , [252, 211, 9]   // yellow
+]);
+
+
 myImage.convert("http://www.steegle.com/images/chrome.jpg");
